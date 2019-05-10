9df22b1a2fcf38f6d9b7087c3554669c23122ae8
dotter100 <dotter102@gmail.com>
2019-05-10 10:09:08 +0200

    added Kilometertarief object also added kilometertarief street

---
+++ Registratie/src/main/Java/Shared/Models/Kilometertarief.java
@@ -0,0 +1,83 @@
+package Shared.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Kilometertarief {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int price;
+
+    private Date start;
+    private Date expire;
+
+    public Kilometertarief(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Kilometertarief(String name, int price, Date start) {
+        this.name = name;
+        this.price = price;
+        this.start = start;
+    }
+
+    public Kilometertarief(Date expire,String name, int price ) {
+        this.name = name;
+        this.price = price;
+        this.expire = expire;
+    }
+
+    public Kilometertarief(String name, int price, Date start, Date expire) {
+        this.name = name;
+        this.price = price;
+        this.start = start;
+        this.expire = expire;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getExpire() {
+        return expire;
+    }
+
+    public void setExpire(Date expire) {
+        this.expire = expire;
+    }
+}
---
+++ Registratie/src/main/Java/Shared/Models/KilometertariefStreet.java
@@ -0,0 +1,40 @@
+package Shared.Models;
+
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Entity
+public class KilometertariefStreet extends Kilometertarief {
+    private String street;
+
+
+    public KilometertariefStreet(String name, int price,String street) {
+        super(name, price);
+        this.street = street;
+    }
+
+    public KilometertariefStreet(String name, int price, Date start, String street) {
+        super(name, price, start);
+        this.street = street;
+    }
+
+    public KilometertariefStreet(Date expire, String name, int price, String street) {
+        super(expire, name, price);
+        this.street = street;
+    }
+
+    public KilometertariefStreet(String name, int price, Date start, Date expire, String street) {
+        super(name, price, start, expire);
+        this.street = street;
+    }
+
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+}
